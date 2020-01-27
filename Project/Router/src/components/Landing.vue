3ffc0e3a08a524445853a77a82b50fed98b63774
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-01-27 15:03:10 -0700

    Add files via upload
    updated landing page

--- Project/Router/src/components/Landing.vue
+++ Project/Router/src/components/Landing.vue
@@ -1,34 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div id="hello">
+
+    <br>
+    <div id="content">
+      <img src="@/assets/logo.png" id="logo">
+
+
+    </div>
+    <div id="center">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+      ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+      ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+       sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+       sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </div>
+    <div id="topLeft">
+      ACGCA Model
+
+    </div>
   </div>
+
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
-  }
-}
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+
+#hello {
+  background-image: url('../assets/Background.png');
+  min-height: 100%;
+  min-width: 1024px;
+
+  /* Set up proportionate scaling */
+  width: 100%;
+  height: auto;
+
+  /* Set up positioning */
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 18px;
+  color: black;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#logo {
+  top: 0;
+  right: 0;
+  position: absolute;
+  display: block;
+  height: 100px;
+  width: 100px;
 }
-a {
-  color: #42b983;
+
+#content {
+  position: relative;
+}
+
+#topLeft {
+  position: absolute;
+  top: 55px;
+  left: 16px;
+  font-size: 28px;
+  color: white;
 }
 </style>
