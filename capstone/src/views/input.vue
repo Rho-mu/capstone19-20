5457699282e7fa5ebf16cf17271355c67cf1a36a
ForBagels <50093624+ForBagels@users.noreply.github.com>
2020-02-10 14:53:52 -0700

    Create input.vue

---
+++ capstone/src/views/input.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="float:left" id="inputConsole">
+      <div id="menuForInput">
+            <button name="scenario" >
+            <button name="showAll">
+            <button name="Run">
+     </div>
+     <div id="scenarioConsole">
+         Scenario Name:
+         Description:
+     </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
