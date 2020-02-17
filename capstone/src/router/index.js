857a96c6e063ba71ea510212a65aea846b5c8a19
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-02-17 13:27:22 -0700

    Add files via upload
    updated routes

--- capstone/src/router/index.js
+++ capstone/src/router/index.js
@@ -1,55 +1,69 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 import firebase from 'firebase'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import Visualization from '../views/Vis.vue'
+import Survey from '../views/Survey.vue'
+import Landing from '../views/Landing.vue'
+import Inputs from '../views/inputs.vue'
 
-import Home from '@/components/HelloWorld'
-import Login from '@/views/Login'
-import SignUp from '@/views/SignUp'
-import Survey from '@/views/survey'
-import Visualization from '@/views/Vis'
+Vue.use(VueRouter)
 
-
-
-
-Vue.use(Router)
-
-const router = new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/sign-up',
-      name: 'SignUp',
-      component: SignUp
-    },
-    {
-      path: '/survey',
-      name: 'Survey',
-      component: Survey
-    },
-    {
-      path: '/vis',
-      name: 'Vis',
-      component: Visualization
+const routes = [
+  {
+    path: '/survey',
+    name: 'Survey',
+    component: Survey,
+    meta: {
+      requiresAuth: true
     }
-  ]
-});
+  },
+  {
+    path:  '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path:  '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '*', // this redirects anything that doesn't correspond to the login page
+    redirect: '/Landing'
+  },
+  {
+    path: '/visualization', // this redirects anything that doesn't correspond to the login page
+    name: 'Visualization',
+    component: Visualization
+  },
+  {
+    path: '/landing', // this redirects anything that doesn't correspond to the login page
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/inputs', // this redirects anything that doesn't correspond to the login page
+    name: 'Inputs',
+    component: Inputs
+  }
+]
 
-router.beforeEach((to, from, next) => {
-  const currentUser = firebase.auth().currentUser;
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
 
-  if (requiresAuth && !currentUser) next('login');
-  else if (!requiresAuth && currentUser) next('home');
-  else next();
-});
+// router.beforeEach((to, from, next) => {
+//   const currentUser = firebase.auth().currentUser;
+//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+//
+//   if (requiresAuth && !currentUser) next('login');
+//   else if (!requiresAuth && currentUser) next('home');
+//   else next();
+// });
 
 export default router;
