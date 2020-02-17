c180c9900de93d77e7574aec86e7a91118453af7
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-02-17 13:25:34 -0700

    Add files via upload

---
+++ capstone/tutorials/vue-firebase-tutorial/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+
+    <button @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  methods: {
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
+    }
+  }
+
+}
+</script>
