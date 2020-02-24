8e48ed3886beb9d5585f5346bcdcd14cf45a9456
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-02-24 09:50:28 -0700

    Added help buttons to the survey

--- capstone/src/views/Survey.vue
+++ capstone/src/views/Survey.vue
@@ -1,22 +1,37 @@
 <template>
   <div id="app">
     <form @submit="submitName">
-    <div>
-      <img src="../assets/Logo-Black.png"><br>
-      <input type="text" v-model="newUser.first_Name" placeholder="First Name" required><br>
-      <input type="text" v-model="newUser.last_Name" placeholder="Last Name" required><br>
-      <input type="text" v-model="newUser.occupation" placeholder="Affiliation"required><br>
-      <input type="text" v-model="newUser.locale" placeholder="Locale"required><br>
-      <input type="text" v-model="newUser.role" placeholder="Affiliation Role" required><br>
-      <button type="submit" value="Submit">Add</button>
-    </div>
-    <div>
-      <ul>
-        <li v-for="user of users"
-          v-bind:key="user['.key']">
-        </li>
-      </ul>
-    </div>
+      <div>
+        <img src="../assets/Logo-Black.png"><br>
+        <input type="text" v-model="newUser.first_Name" placeholder="First Name" required>
+          <span class="help-tip">
+            <p>helllo</p>
+          </span><br>
+        <input type="text" v-model="newUser.last_Name" placeholder="Last Name" required>
+          <span class="help-tip">
+            <p>helllo</p>
+          </span><br>
+        <input type="text" v-model="newUser.occupation" placeholder="Affiliation"required>
+          <span class="help-tip">
+            <p>helllo</p>
+          </span><br>
+        <input type="text" v-model="newUser.locale" placeholder="Locale"required>
+          <span class="help-tip">
+            <p>helllo</p>
+          </span><br>
+        <input type="text" v-model="newUser.role" placeholder="Affiliation Role" required>
+          <span class="help-tip">
+            <p>helllo</p>
+          </span><br>
+        <button type="submit" value="Submit">Add</button>
+      </div>
+      <div>
+        <ul>
+          <li v-for="user of users"
+            v-bind:key="user['.key']">
+          </li>
+        </ul>
+      </div>
   </form>
   </div>
 </template>
@@ -86,5 +101,76 @@ input {
   margin: 10px 0;
   width: 20%;
   padding: 15px;
+}
+.help-tip{
+  position: absolute;
+  text-align: center;
+  background-color: #BCDBEA;
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+  font-size: 14px;
+  line-height: 26px;
+  cursor: default;
+}
+.help-tip:before{
+  content:'?';
+  font-weight: bold;
+  color:#fff;
+}
+.help-tip:hover p{
+  display: inline;
+  transform-origin: 100% 0%;
+
+  -webkit-animation: fadeIn 0.3s ease-in-out;
+  animation: fadeIn 0.3s ease-in-out;
+}
+.help-tip p{
+  display: none;
+  text-align: left;
+  background-color: #1E2021;
+  padding: 20px;
+  width: 300px;
+  postion: absolute;
+  border-radius: 3px;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.2);
+  right: -4px;
+  color: #fff;
+  font-size: 13px;
+  line-height: 1.4;
+}
+.help-tip p:before{ /* The pointer of the tooltip */
+    position: relative;
+    content: '';
+    width:0;
+    height: 0;
+    border:6px solid transparent;
+    border-bottom-color:#1E2021;
+    right:10px;
+    top:-12px;
+}
+.help-tip p:after{ /* Prevents the tooltip from being hidden */
+    width:100%;
+    height:40px;
+    content:'';
+    position: relative;
+    top:-40px;
+    left:0;
+}
+@-webkit-keyframes fadeIn {
+    0% {
+        opacity:0;
+        transform: scale(0.6);
+    }
+
+    100% {
+        opacity:100%;
+        transform: scale(1);
+    }
+}
+
+@keyframes fadeIn {
+    0% { opacity:0; }
+    100% { opacity:100%; }
 }
 </style>
