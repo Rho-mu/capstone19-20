7ecf72fe6b332282f57f593daccfea7a462c4e7e
Riley McWilliams <rcm264@nau.edu>
2020-04-27 13:20:25 -0700

    Update Landing.vue
    Added Kiona's description.

--- capstone/src/views/Landing.vue
+++ capstone/src/views/Landing.vue
@@ -8,15 +8,55 @@
 
     </div>
     <div id="center">
-      ACGCA Introduction
-
-      The Allometrically Constrained Growth and Carbon Allocation (ACGCA) model is an individually based model of tree growth and mortality originally developed by Ogle and Pacala (2009). As the name implies the ACGCA model makes use of functional traits and allometries between tissue types (i.e. leaves, roots, and stems) to determine carbon allocation in a tree.
-      <br><br>
-      Carbon dynamics within the tree are essential to this model. Carbon is allocated between a structural and two storage pools in a way that satisfies the aforementioned allometric relationships (Ogle and Pacala (2009). One storage pool is associated with leaf and fine root tissue. Labile carbon in this pool is retranslocated when the tissue senesceses (Ogle and Pacala 2009). The other storage pool is in the sapwood and is used in times of stress (high labile carbon demand relative to carbon inputs). Finally there is a transient labile carbon pool representative of recent photosynthate and retranslocated carbon. It is not associated with a specific tissue and is immediately redistributed within the tree to accommodate biomass production, respiration, and allocation to storage. If the labile carbon available to the tree is depleted the tree dies (Ogle and Pacala 2009).
-      <br><br>
-      There are six possible physiological states in the ACGCA model: healthy, static, shrinking, recovering, recovered, or dead (Ogle and Pacala 2009). Each state is described by a set of difference equations satisfying mass-balance relationshipts between structural and labile carbon pools. The only environmental driver included in the model is parmax (annual radiation for a site).
-      <br><br>
-      For a full description of the model see Ogle and Pacala (2009) and the accompanying supplemental material. The model has also been used in several other studies (Fell et al. 2018 and Gemoets et al 2013).
+      <h2>Brief overview of the ACGCA model</h2>
+      <p>
+      The Allometrically Constrained Growth and Carbon Allocation (ACGCA) model is an individual-based model of tree growth and mortality.
+      The ACGCA model makes use of plant functional traits and allometric relationships between different tissue types or organs (i.e. leaves, roots, and stems)
+      to determine how carbon is allocated within the tree to meet growth and maintenance demands.
+      </p>
+      <p>
+      The carbon allocation scheme satisfies both allometric constraints and physiological processes, and is unique and essential to this model.
+      Labile carbon is allocated among structural pools and two storage pools in a way that satisfies allometric relationships.
+      The structural pools include biomass of leaves, fine roots, trunk sapwood, trunk heartwood, other (branches, coarse roots) sapwood, and other heartwood.
+      One labile storage pool is associated with leaf and fine root tissue; when leaves or roots die, labile carbon in these senescing tissues can be drawn-up
+      and used elsewhere within the tree. Labile carbon is also stored in the sapwood, which is used during times of stress (high labile carbon demand relative
+      to carbon inputs). There is also a transient labile carbon pool that contains recent photosynthates and retranslocated carbon; this carbon is immediately
+      translocated and used to meet growth and maintenance demands. If these labile carbon pools are depleted, the tree dies. There are six possible physiological
+      states captured by the ACGCA model: healthy, static, shrinking, recovering, recovered, or dead. Each state is described by a set of difference equations
+      satisfying mass-balance relationships between the structural and labile carbon pools.
+      </p>
+      <p>
+      The ACGCA model requires numerical values for about 30 parameters (model inputs or “functional traits”). Currently, light is the only environmental
+      driver included in the model. The model has been thoroughly tested with parameters representative of red maple (Acer rubrum) and loblolly
+      ping (Pinus taeda).  The model produces time-series of simulation output related to tree height, trunk radius, canopy dimensions,
+      structural biomass of different compartments (leaves, roots, trunk heartwood, trunk sapwood, etc.), physiological state, among many others.
+      </p>
+      <p>
+      This web-based software was developed to provide a user-friendly means of exploring the ACGCA model predictions and learning about the
+      processes and traits governing tree growth.
+      </p>
+      <h3>
+      For a full description of the ACGCA model, see the following paper and its associated supplemental materials:
+      </h3>
+          <p>
+          Ogle, K. and S.W. Pacala (2009) A modeling framework for inferring tree growth and allocation from physiological, morphological,
+          and allometric traits. Tree Physiology, 29:587-605.
+          </p>
+      <h3>
+      For additional information about the model, see:
+      </h3>
+          <p>
+          Gemoets, D., J.J. Barber, andK. Ogle (2013). Reversible jump MCMC for inferencein a deterministic
+          individual-based model of tree growth forstudying forest dynamics. Environmetrics, 24: 433-448.
+          </p>
+          <p>
+          Fell M., J. Barber, J.W. Lichstein, and K. Ogle (2018). Multidimensional trait space informed by a
+          mechanistic model of tree growth and carbon allocation. Ecosphere, 9(1):e02060. abstract
+          </p>
+          <p>
+          Fell, M. and K. Ogle (2018). Refinement of a theoretical trait space for North American trees
+          via environmental filtering. Ecological Monographs, 88:372-384.
+          </p>
       <br><br><br>
       <p class="return">
         <button class="testButton" @click="moveToLogin"id="returnButton">Returning?</button>
