e4b264029eeccaf52e9c6c20cbe660dd1129cbd1
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-04-26 20:58:45 -0700

    refinements to the inputs vis page

--- capstone/src/views/Login.vue
+++ capstone/src/views/Login.vue
@@ -4,13 +4,7 @@
     <h3>Sign In</h3>
     <input type="text" v-model="email" placeholder="Email"><br>
     <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click="login">Connection</button>
-    <p>
-      or Sign In with Google <br>
-      <button @click="socialLogin" class="social-button">
-        <img src="../assets/google-logo.png">
-      </button>
-    </p>
+    <button @click="login">Submit</button>
     <p>You don't have an account? You can <router-link to="/sign-up">create one</router-link></p>
   </div>
 </template>
@@ -36,16 +30,8 @@
             alert('Oops. ' + err.message)
           }
         );
-      },
-      socialLogin() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        firebase.auth().signInWithPopup(provider).then((result) => {
-          this.$router.replace('inputs-vis')
-        }).catch((err) => {
-          alert('Oops. ' + err.message)
-        });
       }
+
     }
   }
 </script>
@@ -71,20 +57,5 @@
   p a {
     text-decoration: underline;
     cursor: pointer;
-  }
-  .social-button {
-    width: 75px;
-    background: white;
-    padding: 10px;
-    border-radius: 100%;
-    box-shadow: 0 2px 4px 0 rgba(0,0,0,0,2);
-    outline: 0;
-    border: 0;
-  }
-  .social-button:active {
-    box-shadow: 0 2px 4px 0 rgba(0,0,0,0,1);
-  }
-  .social-button img {
-    width: 100%;
   }
 </style>
