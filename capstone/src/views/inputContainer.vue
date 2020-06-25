53eac6df9936b17c85ca6211b6711672102cca23
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-06-24 18:16:27 -0700

    disabled certain inputs until another is inputted
    i.e. you cant enter etaB until you enter eta

--- capstone/src/views/inputContainer.vue
+++ capstone/src/views/inputContainer.vue
@@ -87,7 +87,7 @@
           </div><br>
 
           <label> etaB:</label><br>
-          <input type="number" v-model="postBody.etaB" step="any" placeholder="0 < etaB < eta" min="0" required>
+          <input type="number" :disabled="postBody.eta==''" v-model="postBody.etaB" step="any" placeholder="0 < etaB < eta" min="0" required>
           <div class="tooltip">
             <span class="tooltiptext">
               <b>Description: </b>Relative height at which trunk transitions from a neiloid to a paraboloid<br>
@@ -111,7 +111,7 @@
           </div><br>
 
           <label> R0:</label><br>
-          <input type="number" v-model="postBody.r0" step="any" placeholder="0 < R0" min="0" required>
+          <input id="R0" type="number" onfocusout="isValid" v-model="postBody.r0" step="any" placeholder="0 < R0" min="0" required>
           <div class="tooltip">
             <span class="tooltiptext">
               <b>Description: </b>Maximum potential crown radius of a tree with diameter at breast height of 0 cm (i.e., for a tree that is exactly 1.37 m tall)<br>
@@ -123,7 +123,7 @@
           </div><br>
 
           <label> R40:</label><br>
-          <input type="number" v-model="postBody.r40" step="any" placeholder="R0 < R40" min="postBody.r0" required>
+          <input id="R40" type="number" :disabled="postBody.r0==''" v-model="postBody.r40" step="any" placeholder="R0 < R40" min="postBody.r0" required>
           <div class="tooltip">
             <span class="tooltiptext">
               <b>Description: </b>Maximum potential crown radius of a tree with diameter at breast height of 40 cm<br>
@@ -151,7 +151,7 @@
         <div class="panel">
           <br>
           <label> Hmax (m):</label><br>
-          <input type="number" v-model="postBody.hmax" step="any" placeholder="0 < Hmax < 127" min="postBody.r0" max="127" required>
+          <input type="number" :disabled="postBody.r0==''" v-model="postBody.hmax" step="any" placeholder="0 < Hmax < 127" min="postBody.r0" max="127" required>
           <div class="tooltip">
             <span class="tooltiptext">
               <b>Description: </b>Maximum tree height<br>
@@ -305,6 +305,9 @@
 </template>
 
 <script>
+
+
+
   import axios from 'axios'
 
   export default {
@@ -408,6 +411,7 @@
     }, // END: data()
 
     methods: {
+
       resetData() {
         this.postBody.hmax=''
         this.postBody.phih=''
@@ -645,7 +649,6 @@
         )
       }
     }, // END: getData()
-
     mounted() {
       this.initializeWebpage()
     } // END: mounted
