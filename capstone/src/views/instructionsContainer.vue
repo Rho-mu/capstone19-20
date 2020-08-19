40a3c0a78a46692d90d555f04f2e3ff2a3ab62e5
Riley McWilliams <rileycmcwilliams@gmail.com>
2020-08-19 15:08:43 -0700

    Update instructionsContainer.vue

--- capstone/src/views/instructionsContainer.vue
+++ capstone/src/views/instructionsContainer.vue
@@ -17,10 +17,9 @@
         <br>
         <h4>Output Tools</h4>
         <p><b>RINGS:</b> Shows the tree rings and their widths at each year, and distinguishes between heartwood and sapwood.</p>
-        <p><b>TREE:</b> Visualizes the tree each year, in terms of its simulated height, trunk radius, crown dimensions, and status (living vs dead).
+        <p><b>TREE:</b> Visualizes the tree each year, in terms of its simulated height, trunk radius, crown dimensions, and status (living vs dead).</p>
         <p><b>CONE:</b> Changes the tree crown to a cone.</P>
         <p><b>CYLINDER:</b> Changes the tree crown to a cylinder.</P>
-        </p>
         <p><b>DATA:</b> Shows the numerical (simulated) values for a subset of output variables produced by the ACGCA simulation at each year. These output data can also be downloaded (saved) as a csv file.</p>
       </div>
     </div>
